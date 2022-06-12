e1473da48aa6dc89baf8ba241e8c917890804b3c
Rilcantus <rileyracer62@yahoo.com>
2022-06-11 21:00:49 -0700

    added load and logout to auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -57,3 +57,53 @@ def register():
     
     # return the html for the register view
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        # pulls user from database where user name matches
+        # fetchone is a single result
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        
+        # check_p_h hashes password and checked against stored for user
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        
+        # session stores data across requests, stored as cookies
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+    
+    return render_template('auth/login.html')
+
+# before_a_r runs before the view functions
+# checks stored data for user_id, and pulls from database
+# stores pulled info in g.user, else None
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+    
